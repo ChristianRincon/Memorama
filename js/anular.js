f7906211ca7d0417a8bba1d21e7662a1623b964b
Christian Mathías Rincón <112495195+ChristianRincon@users.noreply.github.com>
2023-05-20 06:11:18 -0300

    anular.js

--- js/anular.js
+++ js/anular.js
@@ -1,4 +1,4 @@
-// Evitamos que el usuario pueda ingresar manualmente valores en nuestro input y lo forzamos a hacerlo por medio de los selectores de incremento y decremento.
+// Evitamos que el usuario pueda ingresar manualmente valores en nuestro input y lo forzamos a hacerlo por medio de los selectores de incremento y decremento que vienen por defecto en el <input type="number">
 
 const input = document.getElementById('card-input');
 
@@ -7,3 +7,5 @@ input.addEventListener( 'keydown', function(e){
         e.preventDefault();
     }
 });
+
+// Se evita dicha inserción manual por parte del usuario porque el juego funciona con valores pares de cartas y hay que evitar que el usuario manualmente ingrese valores impares y vulnere el sistema.
